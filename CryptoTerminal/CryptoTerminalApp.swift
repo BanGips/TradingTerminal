615e5068976fb0c9fb72ecf0df5dd9bee7573abe
Sergei.Kastyukovets <bangips2@gmail.com>
2023-01-30 20:45:22 +0300

    add project

---
+++ CryptoTerminal/CryptoTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTerminalApp.swift
+//  CryptoTerminal
+//
+//  Created by Sergei Kastyukovets on 30.01.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
